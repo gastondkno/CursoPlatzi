420c5705d96bcd921653cded9869057c7f44ec0e
gastondkno <106113726+gastondkno@users.noreply.github.com>
2022-05-27 15:21:07 -0300

    version 2

---
+++ valores.js
@@ -0,0 +1,20 @@
+//valor cognitivos
+
+// numeros
+40
+//cadenas
+"Gaston Sterenberg"
+//Boolean
+true
+false
+
+//vacios
+null
+undefined
+
+//valores no primitivos o de objetivos
+
+//array
+[1,2,3]
+//objeto
+{nombre: "Gaston"}
